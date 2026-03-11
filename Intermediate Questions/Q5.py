dfa0d4a0875bf76300df2d9bcfce97449ea0c819
Yukti123456 <90753314+Yukti123456@users.noreply.github.com>
2026-03-12 00:26:40 +0530

    Update Q5.py

--- Intermediate Questions/Q5.py
+++ Intermediate Questions/Q5.py
@@ -1,8 +1,13 @@
 #Q5.Query Parameters: Use requests.get() to search for "Python tutorials" on https://api.example.com/search, passing parameters q=Python tutorials and page=2.
 
 import requests
-url = "https://jsonplaceholder.typicode.com/posts/1"
-r = requests.get(url)
-data = r.json()
-print("Title: ",data["title"])
+
+url = "https://api.example.com/search"
+
+params = {
+    "q": "Python tutorials",
+    "page": 2
+}
+r = requests.get(url, params=params)
+print(r.text)
 
