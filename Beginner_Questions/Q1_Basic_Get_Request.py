60336416b227f2c305eabfd6462d5cc919f13cd5
Yukti123456 <90753314+Yukti123456@users.noreply.github.com>
2026-03-11 22:38:42 +0530

    Update Q1_Basic_Get_Request.py

--- Beginner_Questions/Q1_Basic_Get_Request.py
+++ Beginner_Questions/Q1_Basic_Get_Request.py
@@ -1,1 +1,5 @@
-#
+# Q1-Basic GET Request: Write a Python script using requests to fetch the homepage of https://example.com and print 
+the response status code.
+import requests
+r = requests.get("https://example.com",verify=False)
+print(r.status_code)
