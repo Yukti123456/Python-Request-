8b5814f1b809205cb504eac5c2084643e59e5c52
Yukti123456 <90753314+Yukti123456@users.noreply.github.com>
2026-03-12 10:43:53 +0530

    Update Q6_POST_Request.py

--- Intermediate Questions/Q6_POST_Request.py
+++ Intermediate Questions/Q6_POST_Request.py
@@ -1,1 +1,12 @@
 #Q6. POST Request: Simulate submitting a form to https://example.com/login with JSON data { "username": "user123", "password": "pass123" }.
+
+import requests
+url = "https://httpbin.org/post"
+
+data = {
+    "username": "user123",
+    "password": "pass123"
+}
+
+r = requests.post(url, json=data)
+print(r.json())
